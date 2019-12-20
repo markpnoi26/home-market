48662355b0b1611666f7dd3ff1ccb972a77b48ca
Mark Daniel Delgado <markpnoi26@gmail.com>
2019-12-19 19:58:51 -0800

    added all these files

--- home-market-frontend/src/index.js
+++ home-market-frontend/src/index.js
@@ -1,2 +1,11 @@
 // code goes here
-// more comments on this 
+// more comments on this
+
+let jsonData;
+fetch("http://localhost:3000/farmers")
+  .then(response => {
+    return response.json()
+  })
+  .then(json => {
+    jsonData = json
+  })
